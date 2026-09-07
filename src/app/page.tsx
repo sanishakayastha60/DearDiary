2f9c42c7db81478790693163012e72af9895c48c
Sanisha Kayastha Shrestha <weidade003@gmail.com>
2026-09-07 13:13:49 +0545

    Boilerplate page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import { auth } from "@/auth";
-import SignInButton from "./components/SignInButton";
-import SignOutButton from "./components/SignOutButton";
-import { redirect } from "next/navigation";
-import Quote from "./components/Quote";
+// import { auth } from "@/auth";
+// import SignInButton from "./components/SignInButton";
+// import SignOutButton from "./components/SignOutButton";
+// import { redirect } from "next/navigation";
+// import Quote from "./components/Quote";
 export default async function App() {
-  const session = await auth();
-  if (session?.user?.id) {
-    redirect("/home");
-  }
+  // const session = await auth();
+  // if (session?.user?.id) {
+  //   redirect("/home");
+  // }
   return (
     <main className="ml-[5vw]">
       <h1>Login Page</h1>
